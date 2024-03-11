4174ce5003b1fb3202c322b5d5e4672011f44588
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-03-11 15:40:57 +0000

    Add comments list to each article

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -2,22 +2,44 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import ArticleCard from "./ArticleCard";
+import CommentCard from "./CommentCard";
 
 export default function SingleArticle() {
-  const [articleInfo, setArticleInfo] = useState({});
   const { articleId } = useParams();
+  const [articleInfo, setArticleInfo] = useState({});
+  const [commentsList, setCommentsList] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
-        `https://northcoders-news-03ck.onrender.com/api/articles/${articleId}`
-      )
-      .then(({ data: { article } }) => {
+    const article = axios.get(
+      `https://northcoders-news-03ck.onrender.com/api/articles/${articleId}`
+    );
+    const comments = axios.get(
+      `https://northcoders-news-03ck.onrender.com/api/articles/${articleId}/comments`
+    );
+    Promise.all([article, comments]).then(
+      ([
+        {
+          data: { article },
+        },
+        {
+          data: { comments },
+        },
+      ]) => {
         setArticleInfo(article);
-      });
+        setCommentsList(comments);
+      }
+    );
   }, []);
 
   return (
-    <ArticleCard article={articleInfo}/>
+    <>
+      <ArticleCard article={articleInfo} />
+      <h2>Comments</h2>
+      <ul className="comments-list">
+        {commentsList.map((comment) => {
+          return <CommentCard comment={comment} key={comment.comment_id} />;
+        })}
+      </ul>
+    </>
   );
 }
