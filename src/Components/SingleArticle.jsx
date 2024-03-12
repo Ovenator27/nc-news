1ad7d5a76b3ac37876919b7fee49ab704a56fb92
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-03-12 10:30:18 +0000

    Add functionality to comment on articles

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -1,17 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import ArticleCard from "./ArticleCard";
 import CommentCard from "./CommentCard";
+import UserContext from "../Contexts/SignedInUser";
 
 export default function SingleArticle() {
   const { articleId } = useParams();
   const [articleInfo, setArticleInfo] = useState([]);
   const [commentsList, setCommentsList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
-
+  const [isLoading, setIsLoading] = useState(true);
+  const { signedInUser } = useContext(UserContext);
+  const [commentInput, setCommentInput] = useState("");
+  
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     const article = axios.get(
       `https://northcoders-news-03ck.onrender.com/api/articles/${articleId}`
     );
@@ -29,20 +32,59 @@ export default function SingleArticle() {
       ]) => {
         setArticleInfo([article]);
         setCommentsList(comments);
-        setIsLoading(false)
+        setIsLoading(false);
       }
     );
   }, []);
 
-  return ( isLoading ? <h1>Loading Article</h1> : 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const body = {
+      username: signedInUser.username,
+      body: commentInput,
+    };
+    axios
+      .post(
+        `https://northcoders-news-03ck.onrender.com/api/articles/${articleId}/comments`,
+        body
+      )
+      .then(({data: {comment}}) => {
+        setCommentsList((currList) => {
+          return [comment, ...currList]
+        })
+        setCommentInput("");
+      });
+  }
+
+  return isLoading ? (
+    <h1>Loading Article</h1>
+  ) : (
     <>
       <ArticleCard article={articleInfo[0]} setArticleList={setArticleInfo} />
-      <h2>Comments</h2>
-      <ul className="comments-list">
-        {commentsList.map((comment) => {
-          return <CommentCard comment={comment} key={comment.comment_id} />;
-        })}
-      </ul>
+      <div className="comments-list">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="comment-body">
+            Comment as {signedInUser.username}
+          </label>
+          <br />
+          <textarea
+            className="comment-input"
+            id="comment-body"
+            value={commentInput}
+            placeholder={`Add a comment for ${articleInfo[0].author}`}
+            onChange={(e) => {
+              setCommentInput(e.target.value);
+            }}
+            required
+          ></textarea>
+          <button>Comment</button>
+        </form>
+        <ul>
+          {commentsList.map((comment) => {
+            return <CommentCard comment={comment} key={comment.comment_id} />;
+          })}
+        </ul>
+      </div>
     </>
   );
 }
